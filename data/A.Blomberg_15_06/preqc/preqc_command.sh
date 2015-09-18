7a8b527690a6f293fcd34123e6bc335811159f6e
mtop <mats@topel.se>
2015-09-18 10:24:24 +0200

    Data has moved. Added correct relative path to data

--- data/A.Blomberg_15_06/preqc/preqc_command.sh
+++ data/A.Blomberg_15_06/preqc/preqc_command.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-FILE1=/nobackup/data11/data/Fucus_radicans/A.Blomberg_15_06/best_practice_qc/P1980_101/fastq_trimmed/7_150716_AC6T07ANXX_P1980_101_1.fastq
-FILE2=/nobackup/data11/data/Fucus_radicans/A.Blomberg_15_06/best_practice_qc/P1980_101/fastq_trimmed/7_150716_AC6T07ANXX_P1980_101_2.fastq
+#$ -cwd
+#$ -q high_mem
+#$ -S /bin/bash
+
+FILE1=../best_practice_qc/P1980_101/fastq_trimmed/7_150716_AC6T07ANXX_P1980_101_1.fastq
+FILE2=../best_practice_qc/P1980_101/fastq_trimmed/7_150716_AC6T07ANXX_P1980_101_2.fastq
 PREPROCESSED=Fucus_radicans.preprocessed.fastq
 GENOME=Fucus_radicans.preprocessed.preqc
 
